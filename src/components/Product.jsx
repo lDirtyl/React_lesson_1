2a965c2fa76d2797568e2ffd648dadedce052059
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-11-17 18:52:46 +0100

    next

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,13 @@
-export default function Product() {
-	const price = 999;
-	const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-
+export default function Product({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) {
   return (
-    <>
-      <h2>Tacos</h2>
-	    <img src={imgUrl} alt="Tacos With Lime" width="640" />
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
       <p>Price: {price} credits</p>
-    </>
+    </div>
   );
-};
+}
