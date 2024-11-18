c76f55057abf9257b43b57b611d0a1d673bee192
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-11-18 21:54:35 +0100

    Composition of classes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,20 +1,15 @@
 import './Alert.css';
 
-// const getBgColor = variant => {
-//   switch (variant) {
-//     case 'info':
-//       return 'blue';
-//     case 'success':
-//       return 'green';
-//     case 'error':
-//       return 'red';
-//     case 'warning':
-//       return 'orange';
-//     default:
-//       throw new Error(`Unsupported variant prop value - ${variant}`);
-//   }
-// };
+export const Alert = ({ variant, outlined, elevated, children }) => {
+  const classNames = ['alert', variant];
 
-export const Alert = ({ children }) => {
-  return <p className="alert">{children}</p>;
+  if (outlined) {
+    classNames.push('is-outlined');
+  }
+
+  if (elevated) {
+    classNames.push('is-elevated');
+  }
+
+  return <p className={classNames.join(' ')}>{children}</p>;
 };
