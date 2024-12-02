1357f9150d8199ce2b050ee7b88987f66c3940f3
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-02 01:19:41 +0100

    Оновлення об'єктів

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,24 +41,57 @@ import { useState } from 'react';
 //     </>
 //   );
 // };
-const ClickCounter = ({ value, onUpdate }) => {
-  return <button onClick={onUpdate}>Current: {value}</button>;
-};
+
+// const ClickCounter = ({ value, onUpdate }) => {
+//   return <button onClick={onUpdate}>Current: {value}</button>;
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   // Функція, яку будемо передавати в ClickCounter
+//   // для виклику під час кліку
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   return (
+//     <>
+//       <ClickCounter value={clicks} onUpdate={handleClick} />
+//       <ClickCounter value={clicks} onUpdate={handleClick} />
+//     </>
+//   );
+// };
 
 const App = () => {
-  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
 
-  // Функція, яку будемо передавати в ClickCounter
-  // для виклику під час кліку
-  const handleClick = () => {
-    setClicks(clicks + 1);
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
   };
 
   return (
-    <>
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-    </>
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
   );
 };
 
