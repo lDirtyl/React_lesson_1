10506dd2ca133df12378b05cefc7e1a49854ce1b
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-02 01:09:35 +0100

    Ізоляція стану

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,25 +8,35 @@
 //   );
 // };
 
-import { useState } from 'react';
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Current: {clicks}</button>
+//       <button onClick={handleToggle}>{isOpen ? 'Hide' : 'Show'}</button>
+//       {isOpen && <p>Now you can see me!</p>}
+//     </>
+//   );
+// };
+import ClickCounter from './ClickCounter';
 
 const App = () => {
-  const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
-      <button onClick={handleClick}>Current: {clicks}</button>
-      <button onClick={handleToggle}>{isOpen ? 'Hide' : 'Show'}</button>
-      {isOpen && <p>Now you can see me!</p>}
+      <ClickCounter />
+      <ClickCounter />
     </>
   );
 };
