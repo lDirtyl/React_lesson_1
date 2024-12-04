f17df407ebeca066677119c4118571b3a316b8f8
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-05 00:53:53 +0100

    Етап розмонтування

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,98 +1,54 @@
-// import CustomButton from './CustomButton';
-// import ClickCounter from './ClickCounter';
+import { useState, useEffect } from 'react';
 
-import { useState } from 'react';
-
-// const App = () => {
-//   return (
-//     <>
-//       <CustomButton message="Playing music!">Play some music</CustomButton>
-//       <CustomButton message="Uploading your data!">Upload data</CustomButton>
-//     </>
-//   );
-// };
-
-// const App = () => {
-//   const [clicks, setClicks] = useState(0);
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const handleClick = () => {
-//     setClicks(clicks + 1);
-//   };
-
-//   const handleToggle = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <>
-//       <button onClick={handleClick}>Current: {clicks}</button>
-//       <button onClick={handleToggle}>{isOpen ? 'Hide' : 'Show'}</button>
-//       {isOpen && <p>Now you can see me!</p>}
-//     </>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <>
-//       <ClickCounter />
-//       <ClickCounter />
-//     </>
-//   );
-// };
-
-// const ClickCounter = ({ value, onUpdate }) => {
-//   return <button onClick={onUpdate}>Current: {value}</button>;
-// };
+//
+// ============ Життєвий цикл компонента ========= Хук useEffect ============= Етап монтуванн я================
 
 // const App = () => {
 //   const [clicks, setClicks] = useState(0);
 
-//   // Функція, яку будемо передавати в ClickCounter
-//   // для виклику під час кліку
-//   const handleClick = () => {
-//     setClicks(clicks + 1);
-//   };
+// useEffect(() => {
+//   document.title = `You clicked ${clicks} times`;
+// });
+
+//   useEffect(() => {
+//     console.log('You can see me only once!');
+//   }, []);
 
 //   return (
-//     <>
-//       <ClickCounter value={clicks} onUpdate={handleClick} />
-//       <ClickCounter value={clicks} onUpdate={handleClick} />
-//     </>
+//     <button
+//       onClick={() => {
+//         setClicks(clicks + 1);
+//       }}
+//     >
+//       You clicked {clicks} times
+//     </button>
 //   );
 // };
+
+//
+// =========================Етап розмонтування========================================
+//
+
+const Modal = () => {
+  useEffect(() => {
+    setInterval(() => {
+      console.log(`Interval - ${Date.now()}`);
+    }, 2000);
+  }, []);
+
+  return <div>Modal</div>;
+};
 
 const App = () => {
-  const [values, setValues] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  const updateX = () => {
-    setValues({
-      ...values,
-      x: values.x + 1,
-    });
-  };
-
-  const updateY = () => {
-    setValues({
-      ...values,
-      y: values.y + 1,
-    });
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <p>
-        x: {values.x}, y: {values.y}
-      </p>
-
-      <button onClick={updateX}>Update x</button>
-      <button onClick={updateY}>Update y</button>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+      {isOpen && <Modal />}
     </div>
   );
 };
-
 export default App;
