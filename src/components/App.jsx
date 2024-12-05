428aaa6a34e1c3c0e601c3c9674f074d005fede5
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2024-12-05 01:12:26 +0100

    Робота з LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -103,27 +103,103 @@ import { useState, useEffect } from 'react';
 //   );
 // };
 
+// const App = () => {
+//   const [first, setFirst] = useState(0);
+//   const [second, setSecond] = useState(0);
+
+//   useEffect(() => {
+//     console.log('First updated: ', first);
+//   }, [first]);
+
+//   useEffect(() => {
+//     console.log('Second updated: ', second);
+//   }, [second]);
+
+//   useEffect(() => {
+//     console.log('First or second updated: ', first + second);
+//   }, [first, second]);
+
+//   return (
+//     <>
+//       <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+//       <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+//     </>
+//   );
+// };
+
+//
+// =========================== Робота з LocalStorage ======================================
+//
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     console.log(`Clicks changed - ${clicks}`);
+//   }, [clicks]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     // window.localStorage.setItem('key', JSON.stringify({}));
+
+//     window.localStorage.setItem('saved-clicks', clicks);
+//   }, [clicks]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const [first, setFirst] = useState(0);
-  const [second, setSecond] = useState(0);
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem('saved-clicks');
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    return 0;
+  });
 
   useEffect(() => {
-    console.log('First updated: ', first);
-  }, [first]);
-
-  useEffect(() => {
-    console.log('Second updated: ', second);
-  }, [second]);
-
-  useEffect(() => {
-    console.log('First or second updated: ', first + second);
-  }, [first, second]);
+    window.localStorage.setItem('saved-clicks', clicks);
+  }, [clicks]);
 
   return (
-    <>
-      <button onClick={() => setFirst(first + 1)}>First: {first}</button>
-      <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
-    </>
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
   );
 };
 
