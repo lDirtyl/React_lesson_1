1f9a5d5924da14c27c855e9823d809e218324279
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-06 21:51:31 +0100

    sdfgjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import LoginForm from './components/LoginForm/LoginForm.jsx';
 
 const App = () => {
+  // Колбек-функція для обробки сабміту форми
+  const handleLogin = userData => {
+    // Виконуємо необхідні операції з даними
+    console.log(userData);
+  };
+
   return (
-    <>
-      <LoginForm />
-    </>
+    <div>
+      <h1>Please login to your account!</h1>
+      {/* Передаємо колбек як пропс форми */}
+      <LoginForm onLogin={handleLogin} />
+    </div>
   );
 };
 
